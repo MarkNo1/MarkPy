96a2d76770aef3f2e859d1e4954aa2c43321ffcb
markno1 <marcotreglia1@gmail.com>
2019-04-12 00:05:35 +0200

    Adding Map in neanderthal.

---
+++ src/MarkPy/neanderthal/map.py
@@ -0,0 +1,42 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import Terminal256Formatter
+from pprint import pformat
+
+def colorize(obj):
+    return highlight(pformat(obj), PythonLexer(), Terminal256Formatter())
+
+
+class Map(dict):
+    def __init__(self, *args, **kwargs):
+        super(Map, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.iteritems():
+                    self[k] = v
+
+        if kwargs:
+            for k, v in kwargs.iteritems():
+                self[k] = v
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(Map, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(Map, self).__delitem__(key)
+        del self.__dict__[key]
+
+    def __repr__(self):
+        return colorize(self.__dict__)
+
+    __version__ = 0.8
