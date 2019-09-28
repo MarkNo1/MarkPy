abfbd70929e6c7c66814dc59aa5b4acee4b60a73
Mark <marcotreglia1@gmail.com>
2019-09-28 12:11:24 +0200

    erectus | time : Added second to time()

--- src/MarkPy/erectus/time.py
+++ src/MarkPy/erectus/time.py
@@ -7,6 +7,7 @@ now = lambda : gmtime()
 
 # Time Now Formatted
 time = lambda : strftime('%Y.%m.%d.%H:%M', now())
+time = lambda : strftime('%Y.%m.%d.%H:%M:%S', now())
 
 # Time clock
 clock =  lambda : strftime('%H:%M:%S', now())
