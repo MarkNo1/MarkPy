3e10c8bc9d677c428cd313abffabb05651268ca3
MarkNo1 <marcotreglia1@gmail.com>
2020-09-27 18:37:11 +0200

    basic | atom : version 2

--- MarkPy/basic/atom.py
+++ MarkPy/basic/atom.py
@@ -1,9 +1,14 @@
 from .time import datetime
-from .style import Style
+
+__atom_version__ = 2
 
 class Atom:
     def __init__(self, name, type):
         self.__name__ = name
         self.__type__ = type
         self.__create_date__ = datetime()
-        self._style_ = Style()
+        self.__version__ = __atom_version__
+
+    def newAtom(self, name, version):
+        self.__name__ += f'.{name}'
+        self.__version__ = f'{version}.{self.__version__}'
