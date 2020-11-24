4e2a4d3bdb17cd80de128ba851b2003ace0534ff
HorusElohim <horuselohim@gmail.com>
2020-11-24 20:18:02 +0100

    uploading wheels instead that source code

--- markipy/script/markipy.py
+++ markipy/script/markipy.py
@@ -14,7 +14,7 @@ def Main():
 
     if args.pip_up:
         py = args.pip_up
-        p_build = Process(file_log='Dist.Build', cmd=[py, 'setup.py', 'bdist'], console=True)
+        p_build = Process(file_log='Dist.Build', cmd=[py, 'setup.py', 'sdist'], console=True)
         p_upload = Process(file_log='Pip.Upload', cmd=[py, '-m', 'twine', 'upload', 'dist/*'], console=True)
 
         p_build.start()
