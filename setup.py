10f95d1fce45b7bc8302fc83a83e33eb883da5ae
Mark <horuselohim@gmail.com>
2020-12-02 11:14:02 +0100

    New MANIFEST.in for better pip installation

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='markipy',
     author='Marco T.',
     url='https://github.com/MarkNo1/MarkPy',
-    version='0.1.5',
+    version='0.1.6',
     license=license,
     description='MarkPy LIB',
     long_description=readme,
@@ -23,6 +23,6 @@ setup(
     entry_points={'console_scripts': [f'{PKG_NAME} = {PKG_NAME}.script:Main']},
     install_requires=requirements,
     include_package_data=True,
-    package_data={ PKG_NAME: ['gui/resource/*', 'gui/views/qml/*', 'gui/views/qml/components/*']},
+    package_data={PKG_NAME: ['gui/resource/*', 'gui/views/qml/*', 'gui/views/qml/components/*']},
     python_requires='>=3.7'
 )
